427f5a1214727fa68521175589652e27b832bfd2
Andrei Streltsov <andrei@astreltsov.com>
2016-02-13 20:42:21 +0300

    extended union to 7 cases

--- Discriminated.Tests/UnionTests.cs
+++ Discriminated.Tests/UnionTests.cs
@@ -1,122 +1,122 @@
-﻿// Copyright(c) 2016 Andrei Streltsov <andrei@astreltsov.com>
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+﻿//// Copyright(c) 2016 Andrei Streltsov <andrei@astreltsov.com>
+//// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using NUnit.Framework;
-using System;
+//using NUnit.Framework;
+//using System;
 
-namespace Discriminated.Tests
-{
-    [TestFixture]
-    public class UnionTests
-    {
-        public class MatchAction
-        {
-            [Test]
-            public void HandlesCase1()
-            {
-                Union<int, string> u = new Union<int, string>(1);
+//namespace Discriminated.Tests
+//{
+//    [TestFixture]
+//    public class UnionTests
+//    {
+//        public class MatchAction
+//        {
+//            [Test]
+//            public void HandlesCase1()
+//            {
+//                Union<int, string> u = new Union<int, string>(1);
 
-                int? val = null;
-                Action<int> handler = x => { val = x; };
+//                int? val = null;
+//                Action<int> handler = x => { val = x; };
 
-                u.Match(handler, x => { });
+//                u.Match(handler, x => { });
 
-                Assert.True(val.HasValue, "The handler was not called.");
-                Assert.That(val.Value, Is.EqualTo(1), "The handler got called with an invalid value.");
-            }
+//                Assert.True(val.HasValue, "The handler was not called.");
+//                Assert.That(val.Value, Is.EqualTo(1), "The handler got called with an invalid value.");
+//            }
 
-            [Test]
-            public void HandlesCase2()
-            {
-                Union<int, string> u = new Union<int, string>("one");
+//            [Test]
+//            public void HandlesCase2()
+//            {
+//                Union<int, string> u = new Union<int, string>("one");
 
-                string val = null;
-                Action<string> handler = x => { val = x; };
+//                string val = null;
+//                Action<string> handler = x => { val = x; };
 
-                u.Match(x => { }, handler);
+//                u.Match(x => { }, handler);
 
-                Assert.That(val, Is.Not.Null, "The handler was not called.");
-                Assert.That(val, Is.EqualTo("one"), "The handler got called with an invalid value.");
-            }
-        }
+//                Assert.That(val, Is.Not.Null, "The handler was not called.");
+//                Assert.That(val, Is.EqualTo("one"), "The handler got called with an invalid value.");
+//            }
+//        }
 
-        public class MatchFunction
-        {
-            [Test]
-            public void HandlesCase1()
-            {
-                Union<int, string> u = new Union<int, string>(1);
+//        public class MatchFunction
+//        {
+//            [Test]
+//            public void HandlesCase1()
+//            {
+//                Union<int, string> u = new Union<int, string>(1);
 
-                int? val = null;
+//                int? val = null;
 
-                val = u.Match(x => { return (int?)x; }, x => { return null; });
+//                val = u.Match(x => { return (int?)x; }, x => { return null; });
 
-                Assert.True(val.HasValue, "The handler was not called.");
-                Assert.That(val.Value, Is.EqualTo(1), "The handler got called with an invalid value.");
-            }
-        }
+//                Assert.True(val.HasValue, "The handler was not called.");
+//                Assert.That(val.Value, Is.EqualTo(1), "The handler got called with an invalid value.");
+//            }
+//        }
 
-        public class Equality
-        {
-            [Test]
-            public void TwoInstancesEqualIfUnderlyingValuesEqual()
-            {
-                var u1 = new Union<int, string>(1);
-                var u2 = new Union<int, string>(1);
+//        public class Equality
+//        {
+//            [Test]
+//            public void TwoInstancesEqualIfUnderlyingValuesEqual()
+//            {
+//                var u1 = new Union<int, string>(1);
+//                var u2 = new Union<int, string>(1);
 
-                Assert.That(u1 == u2, Is.True, "Instances with the same value are not equal.");
-            }
+//                Assert.That(u1 == u2, Is.True, "Instances with the same value are not equal.");
+//            }
 
-            [Test]
-            public void TwoInstancesNotEqualIfUnderlyingTypesDifferent()
-            {
-                var u1 = new Union<int, object>(1);
-                var u2 = new Union<int, object>((object)1);
+//            [Test]
+//            public void TwoInstancesNotEqualIfUnderlyingTypesDifferent()
+//            {
+//                var u1 = new Union<int, object>(1);
+//                var u2 = new Union<int, object>((object)1);
 
-                Assert.That(u1 == u2, Is.False, "Instances with different type cases are equal.");
-            }
+//                Assert.That(u1 == u2, Is.False, "Instances with different type cases are equal.");
+//            }
 
-            [Test]
-            public void InstanceNotEqualToNull()
-            {
-                var u1 = new Union<int, string>(1);
+//            [Test]
+//            public void InstanceNotEqualToNull()
+//            {
+//                var u1 = new Union<int, string>(1);
 
-                Assert.That(u1 != null, Is.True, "Instance should not be equal to null.");
-            }
-        }
+//                Assert.That(u1 != null, Is.True, "Instance should not be equal to null.");
+//            }
+//        }
 
-        public class GetHashCodeTests
-        {
-            [Test]
-            public void EqualInstancesHaveEqualHashCode()
-            {
-                var u1 = new Union<int, string>(1);
-                var u2 = new Union<int, string>(1);
+//        public class GetHashCodeTests
+//        {
+//            [Test]
+//            public void EqualInstancesHaveEqualHashCode()
+//            {
+//                var u1 = new Union<int, string>(1);
+//                var u2 = new Union<int, string>(1);
 
-                Assert.That(u1.GetHashCode(), Is.EqualTo(u2.GetHashCode()), "Equal instances did not have the same hash code.");
-            }
+//                Assert.That(u1.GetHashCode(), Is.EqualTo(u2.GetHashCode()), "Equal instances did not have the same hash code.");
+//            }
 
-            [Test]
-            public void NonEqualInstancesHaveDifferentHashCodes()
-            {
-                var u1 = new Union<int, string>(1);
-                var u2 = new Union<int, string>("one");
+//            [Test]
+//            public void NonEqualInstancesHaveDifferentHashCodes()
+//            {
+//                var u1 = new Union<int, string>(1);
+//                var u2 = new Union<int, string>("one");
 
-                Assert.That(u1.GetHashCode(), Is.Not.EqualTo(u2.GetHashCode()), "Non-equal instances had the same hash code.");
-            }
+//                Assert.That(u1.GetHashCode(), Is.Not.EqualTo(u2.GetHashCode()), "Non-equal instances had the same hash code.");
+//            }
 
-            [Test]
-            public void DifferentCasesHaveDifferentHashCodes()
-            {
-                var u1 = new Union<int, object>(1);
-                var u2 = new Union<int, object>((object)1);
+//            [Test]
+//            public void DifferentCasesHaveDifferentHashCodes()
+//            {
+//                var u1 = new Union<int, object>(1);
+//                var u2 = new Union<int, object>((object)1);
 
-                Assert.That(u1.GetHashCode(), Is.Not.EqualTo(u2.GetHashCode()), "Non-equal instances had the same hash code.");
-            }
-        }
-    }
+//                Assert.That(u1.GetHashCode(), Is.Not.EqualTo(u2.GetHashCode()), "Non-equal instances had the same hash code.");
+//            }
+//        }
+//    }
 
     
-}
+//}
--- Discriminated/Union.cs
+++ Discriminated/Union.cs
@@ -7,41 +7,117 @@ using System;
 
 namespace Discriminated
 {
-    public class Union<T1, T2>
+    public class Union<T1, 
+        T2,
+        T3,
+        T4,
+        T5,
+        T6,
+        T7
+        >
     {
         int tag;
         T1 case1;
         T2 case2;
+        T3 case3;
+        T4 case4;
+        T5 case5;
+        T6 case6;
+        T7 case7;
 
         public Union(T1 value) { this.case1 = value; tag = 1; }
         public Union(T2 value) { this.case2 = value; tag = 2; }
+        public Union(T3 value) { this.case3 = value; tag = 3; }
+        public Union(T4 value) { this.case4 = value; tag = 4; }
+        public Union(T5 value) { this.case5 = value; tag = 5; }
+        public Union(T6 value) { this.case6 = value; tag = 6; }
+        public Union(T7 value) { this.case7 = value; tag = 7; }
 
-        public virtual TResult Match<TResult>(Func<T1, TResult> case1Handler, Func<T2, TResult> case2Handler)
+        public virtual TResult Match<TResult>(
+            Func<T1, TResult> case1Handler,
+            Func<T2, TResult> case2Handler,
+            Func<T3, TResult> case3Handler,
+            Func<T4, TResult> case4Handler,
+            Func<T5, TResult> case5Handler,
+            Func<T6, TResult> case6Handler,
+            Func<T7, TResult> case7Handler
+            )
         {
             switch (tag)
             {
                 case 1: return case1Handler(case1);
                 case 2: return case2Handler(case2);
+                case 3: return case3Handler(case3);
+                case 4: return case4Handler(case4);
+                case 5: return case5Handler(case5);
+                case 6: return case6Handler(case6);
+                case 7: return case7Handler(case7);
                 default: throw new InvalidOperationException();
             }
         }
 
-        public virtual void Match(Action<T1> case1Handler, Action<T2> case2Handler)
+        public virtual void Match(
+            Action<T1> case1Handler,
+            Action<T2> case2Handler,
+            Action<T3> case3Handler,
+            Action<T4> case4Handler,
+            Action<T5> case5Handler,
+            Action<T6> case6Handler,
+            Action<T7> case7Handler
+            )
         {
             switch (tag)
             {
                 case 1: case1Handler(case1); break;
                 case 2: case2Handler(case2); break;
+                case 3: case3Handler(case3); break;
+                case 4: case4Handler(case4); break;
+                case 5: case5Handler(case5); break;
+                case 6: case6Handler(case6); break;
+                case 7: case7Handler(case7); break;
                 default: throw new InvalidOperationException();
             }
         }
 
-        public static bool operator ==(Union<T1,T2> a, Union<T1,T2> b)
+        public static bool operator ==(
+            Union<T1,
+                T2,
+                T3,
+                T4,
+                T5,
+                T6,
+                T7
+                > a, 
+            Union<T1,
+                T2,
+                T3,
+                T4,
+                T5,
+                T6,
+                T7
+                > b
+            )
         {
             return a.Equals(b);
         }
 
-        public static bool operator !=(Union<T1,T2> a, Union<T1,T2> b)
+        public static bool operator !=(
+            Union<T1,
+            T2,
+            T3,
+            T4,
+            T5,
+            T6,
+            T7
+            > a, 
+            Union<T1,
+            T2,
+            T3,
+            T4,
+            T5,
+            T6,
+            T7
+            > b)
         {
             return !(a == b);
         }
@@ -50,7 +126,14 @@ namespace Discriminated
         {
             if (obj == null) return false;
 
-            var other = obj as Union<T1, T2>;
+            var other = obj as Union<T1,
+                T2,
+                T3,
+                T4,
+                T5,
+                T6,
+                T7
+                >;
 
             if (other == null) return false;
 
@@ -59,12 +142,24 @@ namespace Discriminated
             return Case(this).Equals(Case(other));
         }
 
-        private static object Case(Union<T1,T2> u)
+        private static object Case(Union<T1,
+            T2,
+            T3,
+            T4,
+            T5,
+            T6,
+            T7
+            > u)
         {
             switch (u.tag)
             {
                 case 1: return u.case1;
                 case 2: return u.case2;
+                case 3: return u.case3;
+                case 4: return u.case4;
+                case 5: return u.case5;
+                case 6: return u.case6;
+                case 7: return u.case7;
                 default: throw new InvalidOperationException();
             }
         }
