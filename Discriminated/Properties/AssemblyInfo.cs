87462d65527eebf3110d25267b968694f27a4457
Andrei Streltsov <andrei@astreltsov.com>
2016-02-19 07:30:38 +0300

    package script done

--- Discriminated/Properties/AssemblyInfo.cs
+++ Discriminated/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Discriminated")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An implementation of a discriminated union data structure with pattern matching support.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andrei Streltsov")]
 [assembly: AssemblyProduct("Discriminated")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Andrei Streltsov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,7 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyInformationalVersion("0.1")]
 
 [assembly: CLSCompliant(true)]
--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,6 @@ var msbuild = require('gulp-msbuild');
 var exec = require('child_process').exec;
 var rm = require('gulp-remove-files');
 
-gulp.task('build-union', function () {
-    return gulp.src('./Discriminated/Discriminated.csproj')
-		.pipe(msbuild({
-		    targets: ['Clean', 'Build'],
-		    properties: { configuration: 'Release' },
-            stdout: false
-		}));
-});
-
 gulp.task('build-generator', function () {
     return gulp.src('./Discriminated.Generator/Discriminated.Generator.csproj')
 		.pipe(msbuild({
@@ -27,12 +18,12 @@ gulp.task('build-generator', function () {
 		}));
 });
 
-gulp.task('remove-generated-files', function () {
+gulp.task('cleanup-gen-files', function () {
     return gulp.src('./Discriminated/*.generated.cs')
         .pipe(rm());
 });
 
-gulp.task('run-generator', function (cb) {
+gulp.task('generate-code', ['build-generator', 'cleanup-gen-files'], function (cb) {
     return exec('Discriminated.Generator\\bin\\Debug\\Discriminated.Generator.exe Discriminated', { cwd : "." },  function(err, stdout, stderr){
         console.log(stdout);
         console.log(stderr);
@@ -40,18 +31,24 @@ gulp.task('run-generator', function (cb) {
     });
 });
 
-gulp.task('build', function (callback) {
-    runSequence(
-      'build-generator',
-      'remove-generated-files',
-      'run-generator',
-      'build-union',
-      function (error) {
-          if (error) {
-              console.log(error.message);
-          } else {
-              console.log('FINISHED SUCCESSFULLY');
-          }
-          callback(error);
-      });
+gulp.task('build', ['generate-code'], function () {
+    return gulp.src('./Discriminated/Discriminated.csproj')
+		.pipe(msbuild({
+		    targets: ['Clean', 'Build'],
+		    properties: { configuration: 'Release' },
+            stdout: false
+		}));
+});
+
+gulp.task('prepare-package', ['build'], function () {
+    return gulp.src('Discriminated/bin/Release/Discriminated.dll')
+        .pipe(gulp.dest('Discriminated/lib'));
+});
+
+gulp.task('package', ['prepare-package'], function (cb) {
+    return exec('nuget.exe pack Discriminated/Discriminated.csproj.nuspec', { cwd: "." }, function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
 });
