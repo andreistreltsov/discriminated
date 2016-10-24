c91ccfe8062e34ca1a7c365358db6ea22c128dc7
Andrei Streltsov <andrei@astreltsov.com>
2016-10-24 11:46:04 +0300

    Added None class to represent a case of empty data.

---
+++ Discriminated/None.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discriminated
+{
+    public class None
+    {
+        public static None Instance = new None();
+    }
+}
