c76a1420d3f9bcff2b83b429960996abde20c21e
Andrei Streltsov <andrei@astreltsov.com>
2016-10-27 11:34:24 +0300

    Added basic support for the Option type.

---
+++ Discriminated/Option.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discriminated
+{
+    public class Option<TValue> : Union<TValue, None>
+    {
+        private Option(TValue val) : base(val) { }
+
+        private Option() : base(Discriminated.None.Instance) { }
+
+        public static Option<TValue> Some(TValue val)
+        {
+            return new Discriminated.Option<TValue>(val);
+        }
+
+        public static Option<TValue> None { get { return new Option<TValue>(); } }
+    }
+}
